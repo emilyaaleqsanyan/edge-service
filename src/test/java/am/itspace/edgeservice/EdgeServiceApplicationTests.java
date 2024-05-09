9d059b009e5bcaaad6d184ff6fe1d93eb1455e37
emilyaaleqsanyan <emilyaaleqsanyan@gmail.com>
2024-05-09 19:19:36 +0400

    Add tiltfile

--- src/test/java/am/itspace/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/am/itspace/edgeservice/EdgeServiceApplicationTests.java
@@ -2,8 +2,6 @@ package am.itspace.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
@@ -22,13 +20,12 @@ class EdgeServiceApplicationTests {
     static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7.0"))
             .withExposedPorts(REDIS_PORT);
 
-    @MockBean
-    ReactiveClientRegistrationRepository clientRegistrationRepository;
+
 
     @DynamicPropertySource
     static void redisProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.redis.host", () -> redis.getHost());
-        registry.add("spring.data.redis.port", () -> redis.getMappedPort(REDIS_PORT));
+        registry.add("spring.redis.host", () -> redis.getHost());
+        registry.add("spring.redis.port", () -> redis.getMappedPort(REDIS_PORT));
     }
 
     @Test
